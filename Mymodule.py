fd561474b005719da8fc09ee7fabc25e783bcc25
akwpython <119290025+Peanutbutt1@users.noreply.github.com>
2024-06-03 07:13:22 +0000

    5th day yesterday not available due to electricity cutoffs

---
+++ Mymodule.py
@@ -0,0 +1,28 @@
+#Now we are going to make our own commands like in math liberary
+
+class Math1:
+    
+    @staticmethod     
+    def add1(x, y):
+        return x + y
+    
+    @staticmethod
+    def sub1(x, y):
+        return x - y
+    
+    @staticmethod
+    def mult1(x, y):
+        return x * y
+    
+    @staticmethod
+    def devide1(x, y):
+        return round(x / y, 2)
+    
+    @staticmethod
+    def average1(x, y):
+        return round((x + y) / 2, 2)
+    
+    @staticmethod
+    def percentage1(x, y):
+        return round((x / y) * 100, 2)
+    
